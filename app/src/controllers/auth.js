eb71b7b4bd908ea35111b1d305fbae14767f277d
luciana <lpiazzi@despegar.com>
2021-10-02 20:52:05 -0300

    lint

--- app/src/controllers/auth.js
+++ app/src/controllers/auth.js
@@ -1,31 +1,30 @@
 require('../middlewares/passport');
 const passport = require('passport');
 
-exports.doLogin = (req, res, next) => {  
+exports.doLogin = (req, res, next) => {
   const authenticator = passport.authenticate('google', {
     scope:
               ['email', 'profile'],
-    }
-  );
+  });
 
-  authenticator(req, res, next)
+  authenticator(req, res, next);
 };
 
 exports.authCallback = (req, res, next) => {
+  // eslint-disable-next-line no-unused-expressions
   passport.authenticate('google', {
     failureRedirect: '/failed',
   }),
-
-  function (req, res) {
+  (function (req, res) {
     const redirectUrl = req.session.redirect;
-    console.log("Redirecting to " + redirectUrl)
+    console.log(`Redirecting to ${redirectUrl}`);
     // Successful authentication, redirect home.
     if (redirectUrl) {
       res.redirect(redirectUrl);
     } else {
       res.redirect('/auth/success');
     }
-  }(req, res, next)
+  }(req, res, next));
 };
 
 exports.logout = (req, res) => {
