66843d43f72d2aa303a62e7fffc040a0027f2010
luciana <lpiazzi@despegar.com>
2021-10-11 14:26:50 -0300

    login integration with microservices

--- app/src/controllers/auth.js
+++ app/src/controllers/auth.js
@@ -2,6 +2,24 @@ require('../middlewares/passport');
 const passport = require('passport');
 const axios = require('axios');
 
+const postUser = (user) => {
+  console.log(`Registering ${user.displayName}`);
+  axios({
+    method: 'post',
+    url: `${process.env.BACK_PY}/api/v1/users/`,
+    data: {
+      first_name: user.name.givenName,
+      last_name: user.name.familyName,
+      email: user.emails[0].value,
+      is_admin: false,
+    },
+  }).then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+  });
+};
+
 exports.doLogin = (req, res, next) => {
   const authenticator = passport.authenticate('google', {
     scope:
@@ -12,13 +30,14 @@ exports.doLogin = (req, res, next) => {
 };
 
 exports.authenticateCallback = (req, res) => {
-  console.log(req.user);
   const redirectUrl = req.session.redirect;
   if (redirectUrl) {
     console.log(`Redirecting to ${redirectUrl}`);
     // Successful authentication, redirect home.
+    postUser(req.user);
     res.redirect(redirectUrl);
   } else {
+    postUser(req.user);
     res.redirect('/auth/success');
   }
 };
