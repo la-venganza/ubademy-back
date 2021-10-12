3335c6b5b50ba04ee249453a11b605b3b61226f2
luciana <lpiazzi@despegar.com>
2021-10-12 17:25:00 -0300

    solving test coverage

--- app/__test__/auth.spec.js
+++ app/__test__/auth.spec.js
@@ -1,11 +1,22 @@
 const authController = require('../src/controllers/auth');
+const axios = require('axios');
+
+jest.mock('axios', () => jest.fn(() => Promise.resolve('teresa teng')));
 
 describe('Auth controller', () => {
   describe('authCallback', () => {
     it('redirects', () => {
       const redirectMock = jest.fn();
+      const user = {
+        displayName: 'Luciana',
+        name: {
+          givenName: 'L',
+          familyName: 'S',
+        },
+        emails: [{ value: 'asdfads' }],
+      };
       const sessionMock = { redirect: redirectMock };
-      const req = { session: sessionMock };
+      const req = { session: sessionMock, user };
       const res = { redirect: redirectMock };
 
       authController.authenticateCallback(req, res);
