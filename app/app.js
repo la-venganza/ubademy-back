5f936ff10df5b24d0b0baaa08d43d0adbae62b6d
agustin <agustin.contini@despegar.com>
2021-10-13 21:15:47 -0300

    change Dockerfile path and change files to work with this new path

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const tracer = require('dd-trace').init();
 const passport = require('passport');
 const cookieSession = require('cookie-session');
 const swaggerUi = require('swagger-ui-express');
@@ -8,18 +7,12 @@ const swaggerDocument = require('./swagger.json');
 require('dotenv').config();
 const routes = require('./src/routes');
 
-// const ddOptions = {
-//   response_code: true,
-//   tags: ['app:ubademy_back'],
-// };
-
-// const connectDatadog = require('connect-datadog')(ddOptions);
 const tracer = require('dd-trace').init();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-// app.use(connectDatadog);
+
 app.use(cookieSession({
   name: 'google-auth-session',
   keys: ['key1', 'key2'],
