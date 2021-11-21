466d22ce735fe8574c893cef0ff32acca0a0f98f
German-Rotili <german.rotili@gmail.com>
2021-11-21 09:24:31 -0300

    Update de rutas de examen

--- app/src/routes/exam.js
+++ app/src/routes/exam.js
@@ -32,4 +32,52 @@ router.post('/', async function(req, res) {
     }
 })
 
+router.patch('/', async function(req, res) {
+    try {
+        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+
+        const response = await examService.patchExam(req.body)
+
+        res.status(202).send(response)
+    } catch (e) {
+        const body = {
+            error: e.name,
+            message: e.message
+        }
+        if (e instanceof ConnectionError) {
+            res.status(500).send(body)
+        } else if (e instanceof AuthError) {
+            res.status(401).send(body)
+        } else if (e instanceof ServerError) {
+            res.status(e.status).send(body)
+        } else {
+            res.status(500).send(body)
+        }
+    }
+})
+
+router.get('/', async function(req, res) {
+    try {
+        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+
+        const response = await examService.getExam(req.body)
+
+        res.status(200).send(response)
+    } catch (e) {
+        const body = {
+            error: e.name,
+            message: e.message
+        }
+        if (e instanceof ConnectionError) {
+            res.status(500).send(body)
+        } else if (e instanceof AuthError) {
+            res.status(401).send(body)
+        } else if (e instanceof ServerError) {
+            res.status(e.status).send(body)
+        } else {
+            res.status(500).send(body)
+        }
+    }
+})
+
 module.exports = router;
