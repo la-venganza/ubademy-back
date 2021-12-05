567d83019206943e5c2260c5b6c7df5444a865a9
German-Rotili <german.rotili@gmail.com>
2021-12-05 16:35:36 -0300

    Agregado swagger deinition

--- app/src/routes/exam.js
+++ app/src/routes/exam.js
@@ -62,7 +62,7 @@ router.patch('/:id', async function(req, res) {
 
         const response = await examService.gradeExam(req.body, req.params.id)
 
-        res.status(201).send(response)
+        res.status(202).send(response)
     } catch (e) {
         const body = {
             error: e.name,
