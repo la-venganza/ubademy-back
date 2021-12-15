38c0fcbf2169dd47847315cbd526fd2b54341f2a
German-Rotili <german.rotili@gmail.com>
2021-12-14 23:53:42 -0300

    Agrego localhost como origin

--- app/app.js
+++ app/app.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  "origin": "*",
+  "origin": ["*", "http://localhost:8080/"],
   "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
   // other options
 }
