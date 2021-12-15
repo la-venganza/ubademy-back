5c82801d078ec9893eb00506a656de9f90af5353
German-Rotili <german.rotili@gmail.com>
2021-12-14 23:14:26 -0300

    Agrego origin *

--- app/app.js
+++ app/app.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}));
 app.options('*', cors())
 
 app.use(
