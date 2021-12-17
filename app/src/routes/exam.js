9348d623b2df3a7de2755c4473d588864da00b99
German-Rotili <german.rotili@gmail.com>
2021-12-17 10:03:41 -0300

    Limpio debugging

--- app/src/routes/exam.js
+++ app/src/routes/exam.js
@@ -130,7 +130,7 @@ router.get('/:exam_id/course/:course_id/lesson/:lesson_id/user/:user_id', async 
 
 router.get('/:exam_id/course/:course_id/lesson/:lesson_id/solution/:exam_taken_id', async function(req, res) {
     try {
-        // const uid = await verifyIdToken(req.cookies.firebaseAuth)
+        const uid = await verifyIdToken(req.cookies.firebaseAuth)
             
         const response = await examService.getExamByTakenId(req.params, req.query)
 
