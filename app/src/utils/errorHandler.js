cc90857cce14a1f336e4a26b76ecef08cdce3d3b
German-Rotili <german.rotili@gmail.com>
2021-12-18 20:26:25 -0300

    Consigue error del back

--- app/src/utils/errorHandler.js
+++ app/src/utils/errorHandler.js
@@ -4,11 +4,12 @@ const ServerError = require('../errors/serverError')
 function handleError(error){
     if (error.response) {
         //custom errors for server status response
+        console.log(error.response.status)
         parsedError = JSON.parse(JSON.stringify(error.response.data))
-        let message = '' + parsedError.detail.type + ' | ' + parsedError.detail.msg + ' | ' + error.status
+        let message = '' + parsedError.detail + ' | ' + error.response.status
         console.log("there was a response")
         console.log(message)
-        throw new ServerError(error, message, error.status || 500)
+        throw new ServerError(error, message, error.response.status || 500)
     } else if (error.request) {
         //custom error for unresponsive server
         console.log("no response from python service")
