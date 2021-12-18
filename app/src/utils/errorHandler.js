23bcc88dedba13699e104bb6d56cd2aabacd0932
German-Rotili <german.rotili@gmail.com>
2021-12-18 20:39:18 -0300

    Rescato solo el mensaje del error 422

--- app/src/utils/errorHandler.js
+++ app/src/utils/errorHandler.js
@@ -7,7 +7,7 @@ function handleError(error){
         parsedError = JSON.parse(JSON.stringify(error.response.data))
         let message = ''
         if (error.response.status == 422) {
-            message = parsedError.detail.msg + ' | ' + error.response.status
+            message = parsedError.detail[0].msg + ' | ' + error.response.status
         } else {
             message = parsedError.detail + ' | ' + error.response.status
         }
