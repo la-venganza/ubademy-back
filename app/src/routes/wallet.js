d489d7093698f909e43a62202879b0f51e4d3198
recheconea <recheconea@sparkdigital.com>
2021-12-19 14:12:11 -0300

    corrected and added auth middleware

--- app/src/routes/wallet.js
+++ app/src/routes/wallet.js
@@ -1,226 +1,207 @@
 const express = require('express');
-const walletService = require('../middlewares/walletService')
-const verifyIdToken =  require('../middlewares/firebase')
-const ConnectionError = require('../errors/connectionError')
-const AuthError = require('../errors/authError')
-const ServerError = require('../errors/serverError')
+const walletService = require('../middlewares/walletService');
+const ConnectionError = require('../errors/connectionError');
+const AuthError = require('../errors/authError');
+const ServerError = require('../errors/serverError');
 
 const router = express.Router();
 
-router.get('/balance/:id', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.get('/balance/:id', async (req, res) => {
+  try {
+    const response = await walletService.getBalance(req.params.id);
 
-        const response = await walletService.getBalance(req.params.id)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.post('/deposit', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.post('/deposit', async (req, res) => {
+  try {
+    const response = await walletService.deposit(req.uid, req.body);
 
-        const response = await walletService.deposit(uid, body)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.get('/:id', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.get('/:id', async (req, res) => {
+  try {
+    const response = await walletService.getWallet(req.params.id);
 
-        const response = await walletService.getWallet(req.params.id)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.post('/:id', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.post('/:id', async (req, res) => {
+  try {
+    const response = await walletService.createWallet(req.params.id);
 
-        const response = await walletService.createWallet(req.params.id)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.get('/teacherBalance/:id', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.get('/teacherBalance/:id', async (req, res) => {
+  try {
+    const response = await walletService.getTeacherBalance(req.params.id);
 
-        const response = await walletService.getTeacherBalance(req.params.id)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.post('/payTeacher', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.post('/payTeacher', async (req, res) => {
+  try {
+    const response = await walletService.payTeacher(body);
 
-        const response = await walletService.payTeacher(body)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.post('/teacherWithdraw/:id', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.post('/teacherWithdraw/:id', async (req, res) => {
+  try {
+    const response = await walletService.teacherWithdraw(req.params.id, req.body);
 
-        const response = await walletService.teacherWithdraw(req.params.id, req.body)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.get('/admin/balance', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.get('/admin/balance', async (req, res) => {
+  try {
+    const response = await walletService.getOwnerBalance();
 
-        const response = await walletService.getOwnerBalance()
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
-router.get('/admin/withdraw', async function(req, res) {
-    try {
-        const uid = await verifyIdToken(req.cookies.firebaseAuth)
+router.get('/admin/withdraw', async (req, res) => {
+  try {
+    const response = await walletService.ownerWithdraw(req.body);
 
-        const response = await walletService.ownerWithdraw(req.body)
-
-        res.status(200).send(response)
-    } catch (e) {
-        const body = {
-            error: e.name,
-            message: e.message
-        }
-        if (e instanceof ConnectionError) {
-            res.status(500).send(body)
-        } else if (e instanceof AuthError) {
-            res.status(401).send(body)
-        } else if (e instanceof ServerError) {
-            res.status(e.status).send(body)
-        } else {
-            res.status(500).send(body)
-        }
+    res.status(200).send(response);
+  } catch (e) {
+    const body = {
+      error: e.name,
+      message: e.message,
+    };
+    if (e instanceof ConnectionError) {
+      res.status(500).send(body);
+    } else if (e instanceof AuthError) {
+      res.status(401).send(body);
+    } else if (e instanceof ServerError) {
+      res.status(e.status).send(body);
+    } else {
+      res.status(500).send(body);
     }
-})
+  }
+});
 
 module.exports = router;
