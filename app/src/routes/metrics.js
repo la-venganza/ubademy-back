35e5075db99c2f2b99285ff2ff7936bc7b18c97c
luciana <lpiazzi@despegar.com>
2021-12-21 22:18:49 -0300

    log

--- app/src/routes/metrics.js
+++ app/src/routes/metrics.js
@@ -11,14 +11,7 @@ router.get('/users/', async (req, res) => {
   try {
     // Start listing users from the beginning, 1000 at a time.
     const res = listAllUsers().then((allUsers) => {
-      console.log('aaaallll');
-      console.log(allUsers[0].users[0].providerData[0].providerId);
       const googleUsers = allUsers[0].users.filter((user) => user.providerData[0].providerId === 'google.com');
-      console.log({
-        totalUsers: allUsers[0].users.length,
-        googleUsers: googleUsers.length,
-        passwordUsers: allUsers[0].users.length - googleUsers.length,
-      });
       return {
         totalUsers: allUsers[0].users.length,
         googleUsers: googleUsers.length,
